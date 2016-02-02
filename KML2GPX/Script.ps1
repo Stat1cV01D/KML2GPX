6b879627b52668abc7a8d691e0cfdafe830b71c6
Stat1c_V01D <thestat1cvoid91@gmail.com>
2016-02-02 21:41:51 +0700

    Initial commit

---
+++ KML2GPX/Script.ps1
@@ -0,0 +1,56 @@
+﻿$templateGpx = [xml]@'
+<?xml version="1.0" encoding="ISO-8859-1" standalone="yes"?>
+<gpx>
+<trk>
+<trkseg>
+<trkpt lat="NN.NNNNNNNNNNNNN" lon="EE.EEEEEEEEEEEEE">
+</trkpt>
+</trkseg>
+</trk>
+</gpx>
+'@
+
+#<trkpt lat="NN.NNNNNNNNNNNNN" lon="EE.EEEEEEEEEEEEE"></trkpt>
+#<trkpt lat="SS.SSSSSSSSSSSSS" lon="WW.WWWWWWWW"></trkpt>
+#<trkpt lat="SS.SSSSSSSSSSSSS" lon="EE.EEEEEEEEEEEEE"></trkpt>
+#<trkpt lat="NN.NNNNNNNNNNNNN" lon="WW.WWWWWWWW"></trkpt>
+
+$coordinates = @(
+	('north', 'east'),
+	('south', 'west'),
+	('south', 'east'),
+	('north', 'west')
+)
+
+Get-ChildItem "C:\Users\User\Downloads" -Filter '*.kml' |
+Foreach-Object{
+    $file = $_
+	$content = [xml](Get-Content $file.FullName)
+	$resultGpx = $templateGpx.Clone()
+		
+	Try
+	{
+		for ($i = 0; $i -lt 4; $i++)
+		{
+			$coordItem = $coordinates[$i]
+			$node = $resultGpx.CreateElement('trkpt')
+			$node.SetAttribute('lat', $content.kml.GroundOverlay.LatLonBox.SelectSingleNode($coordItem[0]).InnerText)
+			$node.SetAttribute('lon', $content.kml.GroundOverlay.LatLonBox.SelectSingleNode($coordItem[1]).InnerText)
+			$resultGpx.gpx.trk.trkseg.AppendChild($node)
+		}
+		
+		# So that "trkseg" node is not converted to String
+		# I will have to add a child node and then delete it
+		$resultGpx.gpx.trk.trkseg.RemoveChild($resultGpx.gpx.trk.trkseg.FirstChild)
+
+		$iso8859_1 = [System.Text.Encoding]::GetEncoding('ISO-8859-1')
+		$fileName = $file.DirectoryName + "\\" + $file.BaseName+'.gpx'
+		$sw = New-Object System.IO.StreamWriter($fileName, $false, $iso8859_1)
+		$resultGpx.Save($sw)
+		$sw.Close()
+	}
+	Catch
+	{
+		Write-Output "Unable to convert $file : $_", $_.ScriptStackTrace
+	}
+}
---
+++ Readme.txt
@@ -0,0 +1,2 @@
+Simple tool to convert coordinates for one specific purpose
+If you need to add features - request them in the correspionding section of the site
